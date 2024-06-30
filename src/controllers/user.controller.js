4e278086f46ce20a77aac29b9794ed0a6a5aed5b
SubhamKumar101201 <das.subham101201@gmail.com>
2024-06-30 20:14:00 +0530

    Add comment, like, playlist and tweet models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -270,11 +270,15 @@ const changeCurrentPassword = asyncHandler( async (req,res) => {
 
 })
 
+// get current user
+
 const getCurrentUser = asyncHandler( async (req,res) => {
     // for fetching current user details
     return res.status(200)
                 .json(new ApiResponse(200, req.user, "current user fetched successfully"))
 })
+
+// update account details
 
 const updateAccountDetails = asyncHandler( async (req,res) => {
     // for update any details of your account
