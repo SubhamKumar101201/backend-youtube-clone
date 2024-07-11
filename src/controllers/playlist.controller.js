e3306f19a11cf63b2ac30205805e89058eab2bfc
SubhamKumar101201 <das.subham101201@gmail.com>
2024-07-11 23:49:33 +0530

    initiate playlist controller add create , addVideo and removeVideo controller to playlsit controller

---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,157 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {Playlist} from "../models/playlist.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
+
+
+//  create playlist controller
+
+const createPlaylist = asyncHandler(async (req, res) => {
+    const {name, description} = req.body
+
+    if(
+        [name, description].some((field) => field?.trim() === "" ||  field === undefined)
+    ) {
+        throw new ApiError("name and description both fields are required", 400)
+    }
+
+    const playlist = await Playlist.create({
+        name,
+        description,
+        owner: req.user?._id
+    })
+
+    if(!playlist) {
+        throw new ApiError("Failed to create playlist", 500)
+    }
+
+    return res.status(200)
+                .json(new ApiResponse(200, playlist, "Playlist created successfully"))
+    
+})
+
+//  get user playlists controller
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    const {userId} = req.params
+    
+})
+
+//  get playlist by id controller
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    
+})
+
+// add video to playlist controller
+
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.params
+
+    if (!(isValidObjectId(playlistId) || isValidObjectId(videoId))) {
+        throw new ApiError(400, "Invalid VideoId or PlaylistId")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if(!playlist) {
+        throw new ApiError("Playlist not found", 404)
+    }
+
+    const videoFile = await Video.findById(videoId)
+
+    if(!videoFile) {
+        throw new ApiError("Video not found", 404)
+    }
+
+    if(playlist?.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(400, "Only owner can add video to their playlist")
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $addToSet: {videos: videoId}
+        },
+        {new: true}
+    )
+
+    if(!updatedPlaylist) {
+        throw new ApiError("Failed to add video to playlist please try again", 404)
+    }
+
+    return res.status(200)
+                .json(new ApiResponse(200, updatedPlaylist, "Add video to playlist successfully"))
+
+})
+
+ // remove video from playlist controller
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.params
+   
+    if(!(isValidObjectId(playlistId) || isValidObjectId(videoId))) {
+        throw new ApiError(400, "Invalid VideoId or PlaylistId")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    const videoFile = await Video.findById(videoId)
+
+    if(!playlist) {
+        throw new ApiError("Playlist not found", 404)
+    }
+
+    if(!videoFile) {
+        throw new ApiError("Video not found", 404)
+    }
+
+    if(playlist?.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(400, "Only owner can remove video from their playlist")
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $pull: {videos: videoId}
+        },
+        {new: true}
+    )
+
+    if(!updatedPlaylist) {
+        throw new ApiError("Failed to remove video from playlist please try again", 404)
+    }
+
+    return res.status(200)
+                .json(new ApiResponse(200, updatedPlaylist, "Remove video from playlist successfully"))
+
+
+})
+
+//  delete playlist controller
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    
+})
+
+//  update playlist controller
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    const {name, description} = req.body
+    
+})
+
+export {
+    createPlaylist,
+    getUserPlaylists,
+    getPlaylistById,
+    addVideoToPlaylist,
+    removeVideoFromPlaylist,
+    deletePlaylist,
+    updatePlaylist
+}
